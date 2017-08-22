ddb26ae93cefc96a2bdbe7cb0a7dcce3ecea576b
franciscogallardo <fragalcer@gmail.com>
2017-08-23 09:51:09 +1000

    Cool kaleidoscope :)

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // any additional setup code goes here
+
+
 }
 
 function draw() {
     // your "draw loop" code goes here
+
+    for (var x1 = -2160; x1 <= 2160; x1 = x1+25) {
+        push();
+        translate(width/2, height/2);
+        fill(random(0,255), random(0,255), random(0,255), random(0, 80));
+        rotate(x1);
+        triangle(width / 2, height / 2, 60, 40, 40, 80);
+        pop();
+    }
+
+    for (var x1 = -2160; x1 <= 2160; x1 = x1+25) {
+        push();
+        translate(width/2, height/2);
+        fill(random(0,255), random(0,255), random(0,255), random(0, 80));
+        rotate(x1);
+        ellipse(width / 3, height / 3, 620, 400);
+        pop();
+    }
+
+
+
 }
